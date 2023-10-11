8d94e54072976c06b1a7fec76acf78c3124d9849
Irene Sanchez Ruano <irene.sanchez.ruano.al@iespoligonosur.org>
2023-10-11 19:49:42 +0200

    Configuración modelos con relaciones incluidas

--- gestiontareas/models.py
+++ gestiontareas/models.py
@@ -11,7 +11,7 @@ class Usuario(models.Model):
     fecha_registro = models.DateTimeField(default=timezone.now, blank=True, null=True)
     
 class Tarea(models.Model):
-    titulo = models.TextField(max_length=200)
+    titulo = models.TextField(max_length=100)
     descripcion = models.TextField()
     prioridad= models.IntegerField()
     ESTADO=[
@@ -25,26 +25,40 @@ class Tarea(models.Model):
         default='PEND',)
     completada=models.BooleanField(default=False)
     fecha_creacion = models.DateTimeField(default=timezone.now, blank=True, null=True)
-    hora_vencimiento= models.DateTimeField(default=timezone.now, blank=True, null=True)
+    hora_vencimiento= models.TimeField(default=timezone.now)
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="creadores")
+    usuario = models.ManyToManyField(Usuario, through="AsignacionTarea", related_name="usuario2")
 
 class Proyecto(models.Model):
-
     nombre= models.TextField(max_length=200)
     descripcion = models.TextField()
     duracion_estimada= models.FloatField()
     fecha_inicio= models.DateTimeField(default=timezone.now, blank=True, null=True) 
     fecha_finalizacion= models.DateTimeField(default=timezone.now, blank=True, null=True)
+    creador = models.ForeignKey(Usuario, on_delete = models.CASCADE, related_name="creador")
+    usuarios = models.ManyToManyField(Usuario, through="ProyectoAsignado",related_name="usuario")
+    tareas = models.ForeignKey(Tarea, on_delete=models.CASCADE)
 
 class Etiqueta(models.Model):
-    nombre = models.TextField(max_length=200, unique=True)
+    nombre = models.TextField(max_length=200, unique=True, blank=True)
+    tareas = models.ManyToManyField(Tarea, through="EtiquetaAsociada")
 
-class AsignaciónTarea(models.Model):
+class AsignacionTarea(models.Model):
     observaciones=models.TextField()
     fecha_asignacion= models.DateTimeField(default=timezone.now, blank=True, null=True)
+    tareas= models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    usuarios= models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
 class Comentario(models.Model):
     contenido= models.TextField()
     fecha_comentario: models.DateTimeField(default=timezone.now, blank=True, null=True)
+    autor = models.OneToOneField(Usuario, on_delete = models.CASCADE)
+    tareas= models.OneToOneField(Tarea, on_delete=models.CASCADE)
 
+class ProyectoAsignado(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
 
-
+class EtiquetaAsociada(models.Model):
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    etiqueta = models.ForeignKey(Etiqueta, on_delete=models.CASCADE)
