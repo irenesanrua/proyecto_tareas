37aa2cba69ede22f19f9630b339510c593afa0cc
Irene Sanchez Ruano <irene.sanchez.ruano.al@iespoligonosur.org>
2023-10-26 21:49:23 +0200

    Terminado ejercicio 2, 3 y 4 de vistas, url y templates

--- gestiontareas/urls.py
+++ gestiontareas/urls.py
@@ -4,4 +4,7 @@ from .import views
 urlpatterns = [
     path('', views.index,name='index'),
     path('proyectos/listar',views.listar_proyectos, name='lista_proyectos'),
+    path('proyectos/<int:id_proyecto>/tareas/', views.dame_tareas_proyecto_porfecha, name='dame_tareas_proyecto_porfecha'),
+    path('tarea/<int:id_tarea>/usuarios/', views.listar_usuarios_asignados, name='listar_usuarios_asignados'),
+
 ]
--- gestiontareas/views.py
+++ gestiontareas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Proyecto, Tarea
+from .models import Proyecto, Tarea, Usuario, AsignacionTarea
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,3 +11,13 @@ def listar_proyectos(request):
     proyectos=Proyecto.objects.select_related("creador").select_related("tareas").prefetch_related("usuarios")
     proyectos=Proyecto.objects.all()
     return render(request, 'proyecto/lista.html', {"proyectos_mostrar": proyectos})
+
+def dame_tareas_proyecto_porfecha(request, id_proyecto):
+    proyecto = Proyecto.objects.get(id=id_proyecto)
+    tareas = Tarea.objects.filter(proyecto=proyecto).order_by("-fecha_creacion")
+    return render(request, 'tarea/lista.html', {"tareasproyecto_mostrar": tareas})
+
+def listar_usuarios_asignados(request, id_tarea):
+    tarea = Tarea.objects.get(id=id_tarea)
+    asignaciones = AsignacionTarea.objects.filter(tarea=tarea).order_by('fecha_asignacion')
+    return render(request, 'asignaciontarea/listausuarios.html', {'asignaciones': asignaciones, 'tarea': tarea})
