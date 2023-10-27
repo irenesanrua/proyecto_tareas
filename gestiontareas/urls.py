70e7b379e83f7b6bfceaf73636ffa0057e3d6995
Irene Sanchez Ruano <irene.sanchez.ruano.al@iespoligonosur.org>
2023-10-27 19:56:31 +0200

    Terminado hasta el 7 vistas, url y template

--- gestiontareas/urls.py
+++ gestiontareas/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('proyectos/<int:id_proyecto>/tareas/', views.dame_tareas_proyecto_porfecha, name='dame_tareas_proyecto_porfecha'),
     path('tarea/<int:id_tarea>/usuarios/', views.listar_usuarios_asignados, name='listar_usuarios_asignados'),
     path('tareas/usuario/<int:id_usuario>/<str:observaciones>', views.dame_tareas_observaciones, name='dame_tareas_observaciones'),
-
+    path('tareas/anyos', views.mostrar_tareas_completadas_anios, name='mostrar_tareas_completadas_anios'),
+    path('proyectos/<int:id_proyecto>/ultimocomentario', views.dame_ultimo_cliente_que_comento, name='dame_ultimo_cliente_que_comento'),
 ]
--- gestiontareas/views.py
+++ gestiontareas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Proyecto, Tarea, Usuario, AsignacionTarea
+from .models import Proyecto, Tarea, Usuario, AsignacionTarea, Comentario
 from django.db.models import Q
 
 # Create your views here.
@@ -24,4 +24,19 @@ def listar_usuarios_asignados(request, id_tarea):
 
 def dame_tareas_observaciones(request, id_usuario,observaciones):
     asignacionObservacion = AsignacionTarea.objects.filter(observaciones__contains=observaciones).filter(usuario=id_usuario)
-    return render(request, 'asignaciontarea/listatareas_observacion.html', {'asignaciones': asignacionObservacion})
+    return render(request, 'asignaciontarea/listatareas_observacion.html', {'mostrar_asignaciones': asignacionObservacion})
+
+def mostrar_tareas_completadas_anios(request):
+    anyo_inicio = int(request.GET.get('anyo_inicio', 2021))
+    anyo_fin = int(request.GET.get('anyo_fin', 2023))
+    # Filtra las tareas creadas entre los dos años y con estado "Completada"
+    tareas = Tarea.objects.filter(fecha_creacion__year__gte=anyo_inicio, fecha_creacion__year__lte=anyo_fin, tipoestado="COM")
+    return render(request, 'tarea/lista_tareas_entre_anios.html', {'tareas': tareas, 'anyo_inicio': anyo_inicio, 'anyo_fin': anyo_fin})
+
+def dame_ultimo_cliente_que_comento(request, id_proyecto):
+    proyecto = Proyecto.objects.get(id=id_proyecto)
+    # Obtén la última tarea del proyecto
+    ultima_tarea = Tarea.objects.filter(proyecto=proyecto).order_by("-fecha_creacion")[:1].get() #se puede usar latest() para coger el último
+    # Obtén el último comentario en la última tarea
+    ultimo_comentario = Comentario.objects.filter(tarea=ultima_tarea).order_by("-fecha_comentario")[:1].get()
+    return render(request, 'comentarios/ultimo.html', {'proyecto': proyecto, 'mostrar_autor': ultimo_comentario})
